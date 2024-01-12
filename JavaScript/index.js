eec334c9da232fac2cfa310077d4d274ed9333c5
Mariii209 <maritza.gutierrez30@yahoo.com>
2024-01-12 10:43:43 -0800

    inserted a event listener to my form element

--- JavaScript/index.js
+++ JavaScript/index.js
@@ -1,1 +1,13 @@
+function searchingAnswer(event) {
+  event.preventDefault();
 
+  new Typewriter("#answer", {
+    strings: "The answer of your question",
+    autoStart: true,
+    cursor: null,
+    delay: 50,
+  });
+}
+
+let formElement = document.querySelector("form");
+formElement.addEventListener("submit", searchingAnswer);
