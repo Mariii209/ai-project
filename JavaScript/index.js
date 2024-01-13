7afbba81158f48fe90d3fc287625d1122bd790c3
Mariii209 <maritza.gutierrez30@yahoo.com>
2024-01-13 13:39:39 -0800

    inserted the api for shecodes ai

--- JavaScript/index.js
+++ JavaScript/index.js
@@ -1,12 +1,25 @@
+function displayAnswer(response) {
+  new Typewriter("#answer", {
+    strings: response.data.answer,
+    autoStart: true,
+    cursor: null,
+    delay: 10,
+  });
+}
+
 function searchingAnswer(event) {
   event.preventDefault();
 
-  new Typewriter("#answer", {
-    strings: "The answer of your question",
-    autoStart: true,
-    cursor: null,
-    delay: 50,
-  });
+  let userInput = document.querySelector("#question-bar");
+  console.log(userInput.value);
+
+  let apiKey = "7f30420fc505ct92a4f1o960ab77843b";
+  let prompt = `Generate a animal information that the user input ${userInput.value} `;
+  let context = `You're a animal expert with so much knowledge about every animal in the world. Type everything in HTML format and only in 4 lines no more than four lines of code. No heading needed please. Add at the end of the response "SheCodes AI" inside a <strong> element. don't ask for users input. follow instrustions`;
+  let apiUrl = `https://api.shecodes.io/ai/v1/generate?prompt=${prompt}&context=${context}&key=${apiKey}`;
+
+  axios.get(apiUrl).then(displayAnswer);
+  console.log("processing");
 }
 
 let formElement = document.querySelector("form");
